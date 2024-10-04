9a6c2269e2d02bad7935b2417da53ad1496d3772
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-10-04 22:27:16 +0530

    * create infrastructure * added prometheus * create routes for respective ec2 route record

--- variables.tf
+++ variables.tf
@@ -1,8 +1,10 @@
 variable "tools" {
+  default = {
     prometheus = {
-      instance_type = "t3.small"
-      zone_id       = "Z09583601MY3QCL7AJKBT"
-      record_type   = "A"
-      env           = "dev"
-    }
-  }
+  instance_type = "t3.small"
+  zone_id       = "Z09583601MY3QCL7AJKBT"
+  record_type   = "A"
+  env           = "dev"
+}
+}
+}
